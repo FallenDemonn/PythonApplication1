5252c623a7fdeaf4588723a259190ffe61b9cf03
opilane <opilane@HP-CZC2349HT9>
2023-11-16 12:42:58 +0200

    2

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,6 +1,20 @@
-print("Tere tulemast! ")
-kool = input("Mis kool sa opid?: ")
-kursus = int(input("Mis kursusel?: "))
-print ("Tere tulemast kooli " + kool.upper() + "\nOle hea " + str(kursus), ". kuursuse opilaseks!")
-print("Tere tulemast kooli " + kool.upper() + "\nOle hea ", kursus, ". kuursuse opilaseks!")
-print("Tere tulemast kooli {0}\nOle hea {1}. kuursuse opilaseks!".format(kool,kursus))
+#print("Tere tulemast! ");
+#kool = input("Mis kool sa opid?: ");
+#kursus = int(input("Mis kursusel?: "));
+#print("Tere tulemast kooli " + kool.upper() + "\nOle hea " + str(kursus), ". kuursuse opilaseks!");
+#print("Tere tulemast kooli " + kool.upper() + "\nOle hea ", kursus, ". kuursuse opilaseks!");
+#print("Tere tulemast kooli {0}\nOle hea {1}. kuursuse opilaseks!".format(kool,kursus));
+
+#print(type(kool));
+#print(type(kursus));
+
+#arv1 = float(input("Arv 1: "));
+#arv2 = float(input("Arv 2: "));
+#print("{0} + {1} = {2}".format(arv1, arv2, arv1 + arv2))
+#print("{0} - {1} = {2}".format(arv1, arv2, arv1 - arv2))
+#print("{0} * {1} = {2}".format(arv1, arv2, arv1 * arv2))
+#print("{0} / {1} = {2}".format(arv1, arv2, arv1 / arv2))
+#print("{0} % {1} = {2}".format(arv1, arv2, arv1 % arv2))
+#print("{0} ** {1} = {2}".format(arv1, arv2, arv1 ** arv2))
+#print("{0} // {1} = {2}".format(arv1, arv2, arv1 // arv2))
+
